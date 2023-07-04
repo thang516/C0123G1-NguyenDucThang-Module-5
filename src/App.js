6e2aa39bd85207d066a0bace2f0d044c02487658
thang516 <nguyenthangfa2001@gmail.com>
2023-07-04 21:09:39 +0700

    bai tap thi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+import {ProductList} from "./component/ProductList";
+import {CreateProduct} from "./component/CreateProduct";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+
+            <Routes>
+                <Route path='/' element={<ProductList/>} />
+                <Route path='/create' element={<CreateProduct/>} />
+            </Routes>
+
+        </>
+    );
 }
 
 export default App;
